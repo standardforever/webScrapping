3040e74194f7157ba331570181e9b6b242f7dc9d
standardforever <standard.forever123@gmail.com>
2023-05-05 02:02:41 -0400

    added script

---
+++ script.js
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install dependencies
+apt-get update
+apt-get install -yq wget gnupg ca-certificates
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -yq google-chrome-stable
+
+# Install Node.js
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -yq nodejs
+
+# Set up Puppeteer configuration
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
+
+
+npm install
+
